c7975d28c029aa2f601b381fca23948d6c1a8ccc
rwilso1 <rwilso1@ilstu.edu>
2025-03-25 11:32:18 -0900

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
+rom flask import Flask, render_template
 from gpiozero import LED
 app = Flask(__name__)
 led = LED(18) # Change to match your LED!
 @app.route('/')
 def index():
-    return render_template('HTML Code.html')
+return render_template('HTML Code.html')
 @app.route('/toggle/<action>')
 def LEDControl(action):
-    if action == 'on':
-        led.on()
-        return 'LED Turned On'
-    elif action == 'off':
-        led.off()
-    return 'LED Turned Off'
+if action == 'on':
+led.on()
+return 'LED Turned On’
+elif action == 'off':
+led.off()
+return 'LED Turned Off'
 if __name__ == '__main__':
-    app.run(host='10.1.0.4', port=80)
-
+app.run(host='10.1.0.4', port=80)
