cbad2cfdce1098ed60239d55e1117059ef07c201
rwilso1 <rwilso1@ilstu.edu>
2025-03-25 11:30:50 -0900

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from gpiozero import LED
+app = Flask(__name__)
+led = LED(18) # Change to match your LED!
+@app.route('/')
+def index():
+    return render_template('HTML Code.html')
+@app.route('/toggle/<action>')
+def LEDControl(action):
+    if action == 'on':
+        led.on()
+        return 'LED Turned On'
+    elif action == 'off':
+        led.off()
+    return 'LED Turned Off'
+if __name__ == '__main__':
+    app.run(host='10.1.0.4', port=80)
+
